0293806da470fb2e54524aaadac8c7f314917270
nshutiyvonne1@gmail.com <ntwalidavid90@gmail.com>
2025-03-12 07:34:29 +0200

    added a file

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
+import WhatIsICO from "@/components/what-is-ico";
 export default function Home() {
   return (
     <main className="min-h-screen bg-deep-purple text-white overflow-hidden pt-16">
---
+++ components/why-choose-token.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function WhyChooseToken() {
+  return (
+    <div>why-choose-token</div>
+  )
+}
+

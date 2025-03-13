918412ff26189a8cd937b894ab79ae8dbdb796b2
nshutiyvonne1@gmail.com <ntwalidavid90@gmail.com>
2025-03-13 17:50:03 +0200

    faq page made

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Navbar from "@/components/navbar"
-import Hero from "@/components/hero"
-import WhatIsIco from "@/components/what-is-ico"
-import WhyChooseToken from "@/components/why-choose-token"
-import TokenAllocation from "@/components/token-allocation"
-import Roadmap from "@/components/roadmap"
-import TokenDetails from "@/components/token-details"
-import ExecutiveTeam from "@/components/executive-team"
+import Navbar from "@/components/navbar";
+import Hero from "@/components/hero";
+import WhatIsIco from "@/components/what-is-ico";
+import WhyChooseToken from "@/components/why-choose-token";
+import TokenAllocation from "@/components/token-allocation";
+import Roadmap from "@/components/roadmap";
+import TokenDetails from "@/components/token-details";
+import ExecutiveTeam from "@/components/executive-team";
+import Faq from "@/components/faq";
 
 export default function Home() {
   return (
@@ -35,8 +36,9 @@ export default function Home() {
       <section id="team">
         <ExecutiveTeam />
       </section>
-      
+      <section id="faq">
+        <Faq />
+      </section>
     </main>
-  )
+  );
 }
-
